633f87e394e088d135c02c8847c4d8e7495a5643
Gregory Guffroy <g.guffroy@hotmail.fr>
2015-09-22 18:25:18 +0200

    Partie 3 terminée

--- webserver/main.c
+++ webserver/main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 #include "socket.h"
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main() {
 	int serveur = creer_serveur(8080);
-	return serveur;
+	int socket_client;
+	int pid;
+	int taille = 0;
+	char msg[256]="";
+	const char *message_bienvenue = "Bonjour , bienvenu sur mon serveur\n";
+	while((socket_client = accept(serveur,NULL,NULL)) != -1){
+		pid = fork();
+		if(pid == 0){
+			sleep(1);
+			write(socket_client, message_bienvenue, strlen(message_bienvenue));
+			}
+		while((taille = read(socket_client, msg,256)) != -1){
+			if(write(socket_client, msg, strlen(msg)) == -1){
+				perror("Erreur ecriture");
+			}
+		}
+	}
+	return 1;
 }
 
--- webserver/socket.c
+++ webserver/socket.c
@@ -9,8 +9,8 @@
 
 int creer_serveur(int port) {
 	int socket_serveur;
-	int socket_client;
-	const char *message_bienvenue = "Bonjour , bienvenu sur mon serveur\n";
+	//int socket_client;
+	//const char *message_bienvenue = "Bonjour , bienvenu sur mon serveur\n";
 	struct sockaddr_in saddr;
 	saddr.sin_family = AF_INET;
 	saddr.sin_port = htons(port);
@@ -28,11 +28,11 @@ int creer_serveur(int port) {
 	if(listen(socket_serveur, 10) == -1) {
 		perror("listen socket_serveur");
 	}
-
+	/*
 	socket_client = accept(socket_serveur, NULL, NULL);
 	if(socket_client == -1) {
 		 perror("accept");  
-	} 
-	write(socket_client, message_bienvenue, strlen(message_bienvenue));
-	return 1;
+	} */
+	//write(socket_client, message_bienvenue, strlen(message_bienvenue));
+	return socket_serveur;
 }
