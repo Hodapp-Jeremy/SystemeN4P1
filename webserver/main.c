0d3e09d1638f84189f8614f6aec4fed6bb433d2d
Gregory Guffroy <g.guffroy@hotmail.fr>
2015-10-06 17:15:56 +0200

    Question 5.1

--- webserver/main.c
+++ webserver/main.c
@@ -9,15 +9,21 @@ int main() {
   int serveur = creer_serveur(8080);
   int socket_client;
   int pid;
-  int taille = 0;
+  //int taille = 0;
   char msg[256];
   const char *message_bienvenue = "Bonjour , bienvenue sur mon serveur\n";
   while((socket_client = accept(serveur,NULL,NULL)) != -1){
     pid = fork();
     if(pid == 0){
+      FILE *fichier;
+      fichier = fdopen(socket_client,"w+");
       if(write(socket_client, message_bienvenue, strlen(message_bienvenue)) == -1){
 	perror("erreur write");
       }
+      while(fgets(msg,256,fichier) != NULL){
+	fprintf(fichier,"%s %s","<Pawnee>",msg);
+      }
+      /*
       while((taille = read(socket_client, msg,256)) != 0){
 	if(write(socket_client, msg, strlen(msg)) == -1){
 	  perror("Erreur ecriture");
@@ -26,7 +32,8 @@ int main() {
 	  perror("erreur taille");
 	}
       }
-      exit(EXIT_SUCCESS);
+      exit(EXIT_SUCCESS);*/
+      fclose(fichier);
     }
     else{
       close(socket_client);
